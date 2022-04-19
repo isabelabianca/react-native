7d4e35ceb272a9771572e81011f70dc286b71d42
Isabela Bianca <79585893+isabelabianca@users.noreply.github.com>
2022-04-19 09:32:11 -0300

    Iniciando app de Clima

---
+++ weatherApp/components/SearchInput.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import { View, TextInput, StyleSheet } from 'react-native'
+
+export default class App extends React.Component {
+
+  state = {
+    text: '',
+  };
+
+  handleSubmitEditing = _ => {
+    const { onSubmit } = this.props;
+    const {text} = this.state
+    if(!text) return;
+    onSubmit(text);
+    this.setState({text: ''});
+  };
+
+  render() {
+    const { placeholder } = this.props
+    const { text } = this.state
+    return(
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInput}
+          placeholder={placeholder}
+          placeholderTextColor="white" 
+          value={text}
+          onChangeText={(text) => this.setState({text})}
+          onSubmitEditing={this.handleSubmitEditing}/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    marginTop: 20,
+    marginHorizontal: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5
+  },
+  textInput: {
+    flex: 1,
+    color: 'white'
+  }
+})
